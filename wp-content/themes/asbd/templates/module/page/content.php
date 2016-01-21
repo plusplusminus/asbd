1c6dc29bd98cf15fd9b6f107d70b752ea9ddfc51
Iaan van Niekerk <iaan@plusplusminus.co.za>
2016-01-21 13:21:11 +0200

    layouts

--- wp-content/themes/asbd/templates/module/page/content.php
+++ wp-content/themes/asbd/templates/module/page/content.php
@@ -13,25 +13,30 @@
 	$child_pages = new WP_Query( $args );
 ?>
 <div class="page-content">
-	<div class="container">
 	
-		<?php if ( $child_pages->have_posts() ) : $count = 0; ?>
-			<div class="page-content__entry page-content__entry--children">
-				<?php while ( $child_pages->have_posts() ) : $child_pages->the_post(); $count++;?>
-					
-					<?php $page_quote_author = get_post_meta($post->ID,'_asbd_page_quote_author',true); ?>
-					<?php $page_quote_content = get_post_meta($post->ID,'_asbd_page_quote_content',true); ?>
-					<?php $page_title = get_post_meta($post->ID,'_asbd_page_title',true); ?>
-					<?php $page_btn_label = get_post_meta($post->ID,'_asbd_page_btn_label',true); ?>
-					<?php $page_btn_link = get_post_meta($post->ID,'_asbd_page_btn_link',true); ?>
-
-					<div id="child-<?php echo $post->ID;?>" class="page-content__entry--child">
+	<?php if ( $child_pages->have_posts() ) : $count = 0; ?>
+		<div class="page-content__entry page-content__entry--children">
+			<?php while ( $child_pages->have_posts() ) : $child_pages->the_post(); $count++;?>
+				
+				<?php $page_quote_author = get_post_meta($post->ID,'_asbd_page_quote_author',true); ?>
+				<?php $page_quote_content = get_post_meta($post->ID,'_asbd_page_quote_content',true); ?>
+				<?php $page_title = get_post_meta($post->ID,'_asbd_page_title',true); ?>
+				<?php $page_btn_label = get_post_meta($post->ID,'_asbd_page_btn_label',true); ?>
+				<?php $page_btn_link = get_post_meta($post->ID,'_asbd_page_btn_link',true); ?>
+				<?php if (!empty($page_quote_content)):
+					$quote_test = 'hasquote';
+					else :
+					$quote_test = 'noquote';
+					endif;
+				?>
+				<div id="child-<?php echo $post->ID;?>" class="page-content__entry page-content__entry--child page-content__entry--<?php echo $quote_test;?>">
+					<div class="wrapper">
 						<?php if (!empty($page_quote_content)): ?>
-							<div class="entry__quote">
-								<div class="entry__quote--content">
-									<?php echo $page_quote_content; ?>
+							<div class="quote">
+								<div class="quote__content">
+									<?php echo wpautop($page_quote_content); ?>
 								</div>
-								<p class="entry__quote--author"><?php echo $page_quote_author; ?></p>
+								<p class="quote__author"><?php echo $page_quote_author; ?></p>
 							</div><!--/.quote-->
 						<?php endif; ?>
 						<div class="entry__header">
@@ -42,6 +47,7 @@
 								<?php else: ?>
 									<?php the_title(); ?>
 								<?php endif; ?>
+								<?php edit_post_link(); ?>
 							</h2>
 						</div>
 						<div class="entry__content">
@@ -49,33 +55,47 @@
 							<?php if (!empty($page_btn_label)): ?>
 								<a class="entry__content--btn" href="<?php echo $page_btn_link;?>"><?php echo $page_btn_label;?></a>
 							<?php endif; ?>
-						</div>
-					</div><!--/.child-->
-				<?php endwhile; ?>
-			</div><!--/.children-->
-
-		<?php else : ?>
-
-		<div class="page-content__entry">
-			<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>    
-					<div class="entry__header">
-						<h1 class="entry__header--title">
-							<?php if (!empty($page_title)): ?>
-								<?php echo $page_title; ?>
-							<?php else: ?>
-								<?php the_title(); ?>
+							<?php if (!empty($page_btn_label)): ?>
+								<div class="entry__emails">
+									<div class="entry__emails--col"><a class="entry__emails--email" href="mailto:jeff@astorybydesign.com">jeff@astorybydesign.com</a></div>
+									<div class="entry__emails--col"><a class="entry__emails--email" href="mailto:kim@astorybydesign.com">kim@astorybydesign.com</a></div>
+									<div class="entry__emails--col"><a class="entry__emails--email" href="mailto:wayne@astorybydesign.com">wayne@astorybydesign.com</a></div>
+								</div>
 							<?php endif; ?>
-						</h1>
-					</div>
-					<div class="entry__content">
-						<?php the_content(); ?>
-					</div>
-				<?php endwhile; ?>
-			<?php endif; ?>
-		</div><!--/.entry-->
+						</div>
+					</div><!--/.wrapper-->
+				</div><!--/.child-->
+			<?php endwhile; ?>
+		</div><!--/.children-->
 
-		<?php endif; // End IF Statement ?>
+	<?php else : ?>
 	
-	</div><!--/.container-->
+	<?php $page_quote_content = get_post_meta($post->ID,'_asbd_page_quote_content',true); ?>
+	<?php if (!empty($page_quote_content)):
+		$quote_test = 'hasquote';
+		else :
+		$quote_test = 'noquote';
+		endif;
+	?>
+	<div class="page-content__entry page-content__entry--nochild page-content__entry--<?php echo $quote_test;?>">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>    
+				<div class="entry__header">
+					<h1 class="entry__header--title">
+						<?php if (!empty($page_title)): ?>
+							<?php echo $page_title; ?>
+						<?php else: ?>
+							<?php the_title(); ?>
+						<?php endif; ?>
+					</h1>
+				</div>
+				<div class="entry__content">
+					<?php the_content(); ?>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div><!--/.entry-->
+
+	<?php endif; // End IF Statement ?>
+
 </div><!--/.page-content-->
